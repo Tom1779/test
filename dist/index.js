35387d14188bef3bd56098e582b17e6b82b9d66d
Tom Arad <tom.arad@wsu.edu>
2023-06-04 23:47:20 -0700

    Added auto reload to server when code is changed

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const port = 3000;
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Tom's App");
+    res.send("Tom's new App");
 });
 app.listen(port, () => {
     console.log(`now listening on port: ${port}`);
